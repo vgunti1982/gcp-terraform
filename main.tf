5dd6aa16b8b1ceb0caa21aacd5504c64026cdb49
Vijay Gunti <vgunti.1982@gmail.com>
2025-12-13 22:32:30 -0600

    regualr commit

--- main.tf
+++ main.tf
@@ -15,22 +15,82 @@ provider "google" {
   credentials = file("./credentials.json")
 }
 
-# Create a network (optional but recommended)
+# ========================================
+# VPC NETWORK
+# ========================================
 resource "google_compute_network" "vpc_network" {
-  name = "my-vpc-network"
+  name                    = var.vpc_name
+  auto_create_subnetworks = false
+  routing_mode            = "REGIONAL"
+
+  description = "Main VPC network for ${var.project_id}"
 }
 
-# Create a subnet
+# ========================================
+# SUBNET
+# ========================================
 resource "google_compute_subnetwork" "subnet" {
-  name          = "my-subnet"
-  ip_cidr_range = "10.0.1.0/24"
+  name          = var.subnet_name
+  ip_cidr_range = var.subnet_cidr
   region        = var.region
   network       = google_compute_network.vpc_network.id
+
+  private_ip_google_access = true
+  description              = "Subnet in ${var.region}"
 }
 
-# Create a firewall rule to allow SSH
+# ========================================
+# INTERNET GATEWAY (Cloud Router + NAT)
+# ========================================
+resource "google_compute_router" "router" {
+  name    = "${var.vpc_name}-router"
+  region  = var.region
+  network = google_compute_network.vpc_network.id
+
+  description = "Cloud Router for NAT gateway"
+}
+
+resource "google_compute_router_nat" "nat" {
+  name                               = "${var.vpc_name}-nat"
+  router                             = google_compute_router.router.name
+  region                             = google_compute_router.router.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
+}
+
+# ========================================
+# ROUTES
+# ========================================
+# Default route to internet
+resource "google_compute_route" "default_route" {
+  name             = "${var.vpc_name}-default-route"
+  dest_range       = "0.0.0.0/0"
+  network          = google_compute_network.vpc_network.name
+  next_hop_gateway = "default-internet-gateway"
+  priority         = 1000
+}
+
+# Custom route example (optional)
+resource "google_compute_route" "custom_route" {
+  count            = var.create_custom_route ? 1 : 0
+  name             = "${var.vpc_name}-custom-route"
+  dest_range       = var.custom_route_cidr
+  network          = google_compute_network.vpc_network.name
+  next_hop_gateway = "default-internet-gateway"
+  priority         = 1001
+}
+
+# ========================================
+# FIREWALL RULES
+# ========================================
+# Allow SSH from anywhere
 resource "google_compute_firewall" "allow_ssh" {
-  name    = "allow-ssh"
+  name    = "${var.vpc_name}-allow-ssh"
   network = google_compute_network.vpc_network.name
 
   allow {
@@ -38,11 +98,87 @@ resource "google_compute_firewall" "allow_ssh" {
     ports    = ["22"]
   }
 
-  source_ranges = ["0.0.0.0/0"]
+  source_ranges = var.ssh_source_ranges
   target_tags   = ["ssh"]
+  priority      = 1000
+  description   = "Allow SSH access"
 }
 
-# Create a VM Instance
+# Allow HTTP
+resource "google_compute_firewall" "allow_http" {
+  name    = "${var.vpc_name}-allow-http"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["http"]
+  priority      = 1001
+  description   = "Allow HTTP access"
+}
+
+# Allow HTTPS
+resource "google_compute_firewall" "allow_https" {
+  name    = "${var.vpc_name}-allow-https"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["https"]
+  priority      = 1002
+  description   = "Allow HTTPS access"
+}
+
+# Allow custom ports (configurable via tfvars)
+resource "google_compute_firewall" "allow_custom_ports" {
+  name    = "${var.vpc_name}-allow-custom-ports"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = var.custom_ingress_ports
+  }
+
+  source_ranges = var.custom_ports_source_ranges
+  target_tags   = ["custom-ports"]
+  priority      = 1003
+  description   = "Allow custom application ports"
+}
+
+# Allow internal communication
+resource "google_compute_firewall" "allow_internal" {
+  name    = "${var.vpc_name}-allow-internal"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["0-65535"]
+  }
+
+  allow {
+    protocol = "udp"
+    ports    = ["0-65535"]
+  }
+
+  allow {
+    protocol = "icmp"
+  }
+
+  source_ranges = [var.subnet_cidr]
+  priority      = 1004
+  description   = "Allow internal subnet communication"
+}
+
+# ========================================
+# COMPUTE INSTANCE
+# ========================================
 resource "google_compute_instance" "vm_instance" {
   name         = var.instance_name
   machine_type = var.machine_type
@@ -50,8 +186,9 @@ resource "google_compute_instance" "vm_instance" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
-      size  = 20
+      image = var.boot_disk_image
+      size  = var.boot_disk_size
+      type  = var.boot_disk_type
     }
   }
 
@@ -60,24 +197,74 @@ resource "google_compute_instance" "vm_instance" {
     subnetwork = google_compute_subnetwork.subnet.name
 
     access_config {
-      # This gives the VM a public IP
+      nat_ip = var.assign_public_ip ? google_compute_address.vm_static_ip[0].address : null
     }
   }
 
-  tags = ["ssh"]
+  tags = var.instance_tags
 
   metadata = {
     enable-oslogin = "true"
   }
+
+  labels = {
+    environment = var.environment
+    managed_by  = "terraform"
+  }
+
+  depends_on = [google_compute_subnetwork.subnet]
 }
 
-# Output the VM's public IP
+# ========================================
+# STATIC IP ADDRESS (Optional)
+# ========================================
+resource "google_compute_address" "vm_static_ip" {
+  count  = var.assign_public_ip ? 1 : 0
+  name   = "${var.instance_name}-static-ip"
+  region = var.region
+
+  address_type = "EXTERNAL"
+}
+
+# ========================================
+# NETWORK INTERFACE DETAILS
+# ========================================
+output "vpc_network_name" {
+  value       = google_compute_network.vpc_network.name
+  description = "Name of the VPC network"
+}
+
+output "subnet_name" {
+  value       = google_compute_subnetwork.subnet.name
+  description = "Name of the subnet"
+}
+
+output "subnet_cidr" {
+  value       = google_compute_subnetwork.subnet.ip_cidr_range
+  description = "CIDR range of the subnet"
+}
+
+output "router_name" {
+  value       = google_compute_router.router.name
+  description = "Name of the Cloud Router"
+}
+
+output "nat_gateway_name" {
+  value       = google_compute_router_nat.nat.name
+  description = "Name of the NAT gateway"
+}
+
 output "vm_public_ip" {
-  value       = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
-  description = "The public IP of the VM"
+  value       = var.assign_public_ip ? google_compute_address.vm_static_ip[0].address : null
+  description = "Static public IP of the VM"
 }
 
 output "vm_internal_ip" {
   value       = google_compute_instance.vm_instance.network_interface[0].network_ip
-  description = "The internal IP of the VM"
+  description = "Internal IP of the VM"
+}
+
+output "vm_name" {
+  value       = google_compute_instance.vm_instance.name
+  description = "Name of the VM instance"
 }
--- variables.tf
+++ variables.tf
@@ -1,29 +1,121 @@
 variable "project_id" {
-  description = "Your GCP Project ID"
+  description = "GCP Project ID"
   type        = string
-  default     = "your-project-id"  # Replace with your actual GCP project ID
 }
 
 variable "region" {
-  description = "GCP region where resources will be created"
+  description = "GCP region"
   type        = string
   default     = "us-central1"
 }
 
 variable "zone" {
-  description = "GCP zone where the VM will be created"
+  description = "GCP zone"
   type        = string
   default     = "us-central1-a"
 }
 
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
+}
+
+# ========================================
+# VPC & NETWORKING
+# ========================================
+variable "vpc_name" {
+  description = "Name of the VPC network"
+  type        = string
+  default     = "main-vpc"
+}
+
+variable "subnet_name" {
+  description = "Name of the subnet"
+  type        = string
+  default     = "main-subnet"
+}
+
+variable "subnet_cidr" {
+  description = "CIDR range for the subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "create_custom_route" {
+  description = "Whether to create a custom route"
+  type        = bool
+  default     = false
+}
+
+variable "custom_route_cidr" {
+  description = "CIDR range for custom route"
+  type        = string
+  default     = "192.168.0.0/16"
+}
+
+variable "ssh_source_ranges" {
+  description = "CIDR ranges allowed for SSH"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]  # Change to your IP for security
+}
+
+# ========================================
+# COMPUTE INSTANCE
+# ========================================
 variable "instance_name" {
   description = "Name of the VM instance"
   type        = string
-  default     = "my-first-vm"
+  default     = "my-vm"
 }
 
 variable "machine_type" {
   description = "Machine type for the VM"
   type        = string
-  default     = "e2-micro"  # Free tier eligible
+  default     = "e2-micro"
+}
+
+variable "boot_disk_image" {
+  description = "Boot disk image"
+  type        = string
+  default     = "debian-cloud/debian-11"
+}
+
+variable "boot_disk_size" {
+  description = "Boot disk size in GB"
+  type        = number
+  default     = 20
+}
+
+variable "boot_disk_type" {
+  description = "Boot disk type"
+  type        = string
+  default     = "pd-standard"
+}
+
+variable "assign_public_ip" {
+  description = "Whether to assign a static public IP"
+  type        = bool
+  default     = true
+}
+
+variable "instance_tags" {
+  description = "Network tags for the VM"
+  type        = list(string)
+  default     = ["ssh", "http", "https", "custom-ports"]
+}
+
+# ========================================
+# INGRESS PORTS
+# ========================================
+variable "custom_ingress_ports" {
+  description = "List of custom TCP ports to allow ingress"
+  type        = list(string)
+  default     = ["8000", "8089", "9997"]
+}
+
+variable "custom_ports_source_ranges" {
+  description = "CIDR ranges allowed for custom ports"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
 }
